df2c8f87cedcd615fb31beecc7f7d1a0597db518
Cristiano10 <ccristiandarioromero@gmail.com>
2022-01-09 16:52:14 -0300

    add mock & interface

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,28 @@
+import {Task} from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: "aaaaaaaaaaa",
+        day: "Hoy",
+        reminder: true
+    },
+    {
+        id: 2,
+        text: "bbbbbb",
+        day: "ayer",
+        reminder: true
+    },
+    {
+        id: 3,
+        text: "ccccc",
+        day: "mañana",
+        reminder: false
+    },
+    {
+        id: 4,
+        text: "ddddd",
+        day: "en el futuro",
+        reminder: false
+    }
+]
